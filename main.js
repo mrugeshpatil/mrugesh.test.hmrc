a8da070d8413fc645d88490afc3d892b4c13d79f
Mrugesh Patil <patil_mrugesh@yahoo.co.uk>
2019-01-07 11:04:45 +0000

    initial page action goes here

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var HMRC = {} || HMRC;
+
+HMRC.PageSetup = {
+  init: function() {
+    console.log("------- page initial actions goes here");
+  }
+};
+
+//initializze the actions on DOM loads
+HMRC.PageSetup.init();
